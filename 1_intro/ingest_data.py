0e8d891dd187eb23022c0ba07d35c8707f966d18
Adetayo <akinsanyaadetayo@gmail.com>
2024-10-19 23:45:32 +0100

    Load data to DB using docker compose

--- 1_intro/ingest_data.py
+++ 1_intro/ingest_data.py
@@ -1,23 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import os
-import argparse
 
 from time import time
-
 import pandas as pd
 from sqlalchemy import create_engine
 
-
-def main(params):
-    user = params.user
-    password = params.password
-    host = params.host 
-    port = params.port 
-    db = params.db
-    table_name = params.table_name
-    url = params.url
+def main():
+    user = os.getenv('USER')
+    password = os.getenv('PASSWORD')
+    host = os.getenv('HOST')
+    port = os.getenv('PORT')
+    db = os.getenv('DB')
+    table_name = os.getenv('TABLE_NAME')
+    url = os.getenv('URL')
     csv_name = 'output.csv'
 
     # download the CSV file
@@ -36,38 +30,18 @@ def main(params):
 
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
-
     while True:
         try:
             t_start = time()
-
             df = next(df_iter)
-
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
             df.to_sql(name=table_name, con=engine, if_exists='append')
-
             t_end = time()
-
             print('inserted another chunk, took %.3f second' % (t_end - t_start))
         except StopIteration:
             print('completed')
             break
 
-
 if __name__ == '__main__':
-    # Parse the command line arguments and calls the main program
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-    parser.add_argument('--user', help='user name for postgres')
-    parser.add_argument('--password', help='password for postgres')
-    parser.add_argument('--host', help='host for postgres')
-    parser.add_argument('--port', help='port for postgres')
-    parser.add_argument('--db', help='database name for postgres')
-    parser.add_argument('--table_name', help='name of the table where we will write the results to')
-    parser.add_argument('--url', help='url of the csv file')
-
-    args = parser.parse_args()
-
-    main(args)
+    main()
